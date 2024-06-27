0262e5715751f005618d787f4792c1c8650d6cd5
MartimMatos17 <matosmartimmatos@gmail.com>
2024-06-27 16:49:23 +0100

    SPRINT-e03 - Create My Announcements page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-// src/App.js
 import React from 'react';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline } from '@mui/material';
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import HomePage from './components/HomePage';
 import AboutUs from './components/AboutUs';
-
+import MyAnnouncements from './components/MyAnnouncements';
 function App() {
-  return (
-    <>
-      <CssBaseline />
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about-us" element={<AboutUs />} />
-      </Routes>
-    </>
-  );
+return (
+<>
+<CssBaseline />
+<Header />
+<Routes>
+<Route path="/" element={<HomePage />} />
+<Route path="/about-us" element={<AboutUs />} />
+<Route path="/my-announcements" element={<MyAnnouncements />} />
+{/* Adicione outras rotas aqui */}
+</Routes>
+</>
+);
 }
-
 export default App;
