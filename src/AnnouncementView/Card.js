61472c1f6ac2f9a28b8c613268eaa75289ab3ca7
Leonorjp <leonorpocas.14@gmail.com>
2024-06-27 14:00:47 +0100

    Announcement Details page v2

--- src/AnnouncementView/Card.js
+++ src/AnnouncementView/Card.js
@@ -2,6 +2,7 @@ import React from 'react';
 import CarouselIndicators from './CarouselIndicators';
 import Button from './Button';
 import './Card.css';
+import InsertImage from '../images/InsertImage.jpg';
 
 const Card = () => {
   return (
@@ -9,12 +10,13 @@ const Card = () => {
       <div className="card-content">
         <div className="card-header">
         <div className="card-image">
-          <img src="https://via.placeholder.com/200" alt="placeholder" />
-          
+        <img src={InsertImage} alt="description" />
           <CarouselIndicators />
         </div>
         <div className="card-navigation">
+          <div className="backArrow">
             <span className="left-arrow">&lt; back</span>
+          </div>
           </div>
 
       </div> 
@@ -24,11 +26,7 @@ const Card = () => {
       <h1 className="card-title">Name</h1>
       <h2 className="card-category">Category</h2>
       <p className="card-description">
-        Lorem ipsum dolor sit amet. Non delectus accusamus et beatae alias qui quam earum id voluptatem distinctio qui architecto magnam ut galisum libero aut voluptates doloribus. 33 consectetur nihil ut sunt deleniti non sunt esse!
-        Est illo voluptatem est voluptatem rerum id tenetur amet ut corrupti eaque ea fuga consequatur qui porro facere hic laborum animi.
-      </p>
-      <p className="card-description">
-        Est illo voluptatem est voluptatem rerum id tenetur amet ut corrupti eaque ea fuga consequatur qui porro facere hic laborum animi.
+      Lorem ipsum dolor sit amet. Non delectus accusamus et beatae alias qui quam earum id voluptatem distinctio qui architecto magnam ut galisum libero aut voluptates doloribus. 33 consectetur nihil ut sunt deleniti non sunt esse! Est illo voluptatem est voluptatem rerum.
       </p>
     </div>
     <Button text="Claim" />
@@ -36,6 +34,7 @@ const Card = () => {
 </div>
       </div>
     </div>
+    
   );
 };
 
