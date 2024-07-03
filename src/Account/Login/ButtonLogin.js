cab26c03b82a954133012cfd7a9e02022e9e3d09
Leonorjp <leonorpocas.14@gmail.com>
2024-07-03 09:22:04 +0100

    SPRINT-e01---Create-Login-&-Announcements-Details-page

--- src/Account/Login/ButtonLogin.js
+++ src/Account/Login/ButtonLogin.js
@@ -1,23 +1,16 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React from "react";
+import { motion } from "framer-motion";
+import "./ButtonLogin.css";
 
-function ButtonLogin ({ handleExploreClick }) {
+function ButtonLogin({ handleExploreClick }) {
   return (
     <motion.button
-    component={motion.div}
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    onClick={handleExploreClick}
-      style={{
-        width: '80%',
-        padding: '10px',
-        backgroundColor: '#C01722',
-        color: 'white',
-        border: 'none',
-        borderRadius: '5px',
-        cursor: 'pointer',
-        boxSizing: 'border-box'
-      }}
+      className="login-button"
+      variant="contained"
+      component={motion.div}
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      onClick={handleExploreClick}
     >
       Login
     </motion.button>
